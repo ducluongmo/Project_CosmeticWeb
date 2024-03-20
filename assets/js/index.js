5d5e10e579aab2e7485cbd72afeb5548e0a005f4
ducluong <luongduc0401@gmail.com>
2024-03-20 21:44:02 +0700

    put

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,54 @@
-console.log('hi');
+$(document).ready(function(){
+    $(".slider-promotion").owlCarousel({
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        responsive:{
+            0:{
+                items:2,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:2,
+            }
+        }
+    });
+
+    $(".slider-banner").owlCarousel({
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        loop: true,
+        responsive:{
+            0:{
+                items:0,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    $(".slider-new-product").owlCarousel({
+        dots: false,
+        loop: true,
+        margin: 20,
+        responsive:{
+            0:{
+                items:0,
+            },
+            600:{
+                items:4,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });
+  });
---
+++ assets/js/test.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    mouseDrag: false,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        100: {
+            items: 2,
+        }
+    }
+})
